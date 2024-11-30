5d02f8899a8d22ab7fd8290d75a4fd1666e3f18d
AmySuperCoder <tribegriffith@gmail.com>
2024-11-30 07:32:53 -0500

    Initial files to create and populate database

---
+++ seeds.sql
@@ -0,0 +1,35 @@
+INSERT INTO department (name) VALUES
+('Accounting'),
+('IT'),
+('Administration'),
+('Executive');
+
+
+INSERT INTO role (title, salary, department_id) VALUES
+('Lead Accountant', 120000, 1),
+('Book keeper', 70000, 1),
+('Database Administrator', 150000, 2),
+('Director', 200000, 2),
+('Lead Developer', 140000, 2),
+('Developer', 90000, 2),
+('Executive administrator', 65000, 3),
+('Chief Executive Officer', 450000, 4),
+('Chief Financial Officer', 400000, 4),
+('Chief Operating Officer', 400000, 4),
+('Chief Information Officer', 350000, 4);
+
+INSERT INTO employee (first_name, last_name, role_id) VALUES
+('Ann', 'Jones', 1),
+('Bob', 'Smith', 2),
+('Cindy', 'Phillips', 2),
+('Debby', 'Rogers', 3),
+('Edward', 'White', 4),
+('Francine', 'Green', 5),
+('George', 'Black', 6),
+('Heather', 'Brown', 6),
+('Ivan', 'Shoemaker', 7),
+('Janice', 'Lively', 8),
+('Kelsey', 'Harrison', 9),
+('Lori', 'Booker', 10),
+('Micah', 'Morrow', 11);
+
